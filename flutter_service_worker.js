de5caea300adf396e2f1471dff43f1ed3daeb29e
invok3 <taash32@gmail.com>
2022-02-18 13:08:28 +0200

    dd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "904f083443f92f0ef45984f6ab9e7552",
+  "assets/AssetManifest.json": "799e8d062c00a8745ec8eadb7bc687a3",
 "assets/assets/fonts/Cairo-Black.ttf": "c3da5664903bac89a1e5a6f38e47a778",
 "assets/assets/fonts/Cairo-Bold.ttf": "ef55322bc2031017175022b779940cb0",
 "assets/assets/fonts/Cairo-ExtraBold.ttf": "0645b8a99ffdc54a258b3eb7f6d160b9",
@@ -21,6 +21,7 @@ const RESOURCES = {
 "assets/assets/icons/Cart%2520Icon.svg": "220f5aa89905b1351c4fdf71aee29f11",
 "assets/assets/icons/Cash.svg": "9a1bfa4f03729b332bb98439f713637e",
 "assets/assets/icons/Chat%2520bubble%2520Icon.svg": "3e6e1ab6925d3db86ce9bd109c121db0",
+"assets/assets/icons/chats.svg": "3eb4d668f02bda09f3fe610c51ddbfe2",
 "assets/assets/icons/Check%2520mark%2520rounde.svg": "e83caa6ca41e8af9229ae29d79f9410b",
 "assets/assets/icons/Close.svg": "70478a15e0479750b0b03e000041e9f3",
 "assets/assets/icons/Conversation.svg": "291fb6c6acdeb807afb452ae90f930db",
@@ -28,6 +29,7 @@ const RESOURCES = {
 "assets/assets/icons/Error.svg": "0f876e9b9170982e37bbf767c6ebb47f",
 "assets/assets/icons/facebook-2.svg": "ccac513f2d0853c24bf933fc0a6dd8ba",
 "assets/assets/icons/Flash%2520Icon.svg": "8b2bc2fd2894d143940eb565b4a827fe",
+"assets/assets/icons/friends.svg": "9214c32f05d1612639aaf2fe776d032f",
 "assets/assets/icons/Game%2520Icon.svg": "f913dbd8d0e9e437cfb53d774eb980d9",
 "assets/assets/icons/Gift%2520Icon.svg": "6feeb93f33263146741e38f4ebfbccbc",
 "assets/assets/icons/google-icon.svg": "38e282dafbaaf9823263d49349670447",
@@ -40,10 +42,12 @@ const RESOURCES = {
 "assets/assets/icons/Parcel.svg": "45a2e4513281a1c511cff0d7d86439d1",
 "assets/assets/icons/Phone.svg": "589731a88a098c9c6d40e32bc11c3d83",
 "assets/assets/icons/Plus%2520Icon.svg": "23bd873f0fdef239500d68a150f9fa93",
+"assets/assets/icons/profile.svg": "99b1cbb6e0244bc0e8a40efb0222d872",
 "assets/assets/icons/Question%2520mark.svg": "7d0f74b3eb3cbeac772cb6a41476cfcf",
 "assets/assets/icons/receipt.svg": "e0ecaf4c17597903fa1e8ab3c28fa963",
 "assets/assets/icons/reload_icon.svg": "db3915edd53d38cafb33e31cfc7a9e70",
 "assets/assets/icons/remove.svg": "77f17bcf86cb62db1b3ce224d6cb6fd3",
+"assets/assets/icons/rooms.svg": "ae20f45f4a17740240aa8ae37f9b9fb4",
 "assets/assets/icons/Search%2520Icon.svg": "5383aff67a0cc61bc20b953c73d87469",
 "assets/assets/icons/search_icon.svg": "b3f1a6c21c94e65ee935749e4a8059ce",
 "assets/assets/icons/Settings.svg": "d8fd4b8ed70a516c17d3d981d9a49999",
@@ -55,6 +59,7 @@ const RESOURCES = {
 "assets/assets/icons/User%2520Icon.svg": "950d2f1652bdb59675231707a9124535",
 "assets/assets/icons/User.svg": "737d49c1953f8098f618d523b45e5657",
 "assets/assets/icons/wallet.png": "866f4e509e9a95fcb117c1353fc4f408",
+"assets/assets/icons/wallet.svg": "d30c271b2a609cebefa76a1fc749d0f4",
 "assets/assets/images/group_chat.gif": "282f811789fa307f3bb74a5ca9a97c7c",
 "assets/assets/images/group_chat_blue.gif": "8941ec83120281949b43d525f33a6595",
 "assets/assets/images/mobile_encryption.gif": "98ba46c8b568bc3426f03ad4f47a0e86",
@@ -74,9 +79,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0f94a52d793ab1967cb17ab468561bf7",
-"/": "0f94a52d793ab1967cb17ab468561bf7",
-"main.dart.js": "545410f20ebf614b77d11d7d2926b817",
+"index.html": "f7489b0ef45301949cdee228b17d78af",
+"/": "f7489b0ef45301949cdee228b17d78af",
+"main.dart.js": "4665b448cda3e1ab4579eafb8d071af1",
 "manifest.json": "1b221ef5a813a9b3cca2e318e57708b0",
 "version.json": "34d0cc0496223bfce47984f8ef96cac2"
 };
